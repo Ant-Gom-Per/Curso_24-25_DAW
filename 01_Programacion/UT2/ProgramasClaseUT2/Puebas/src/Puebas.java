25dd92d26bcc9326143e406fdf5ee443e2c561d7
Ant-Gom-Per <antgomper79@outlook.es>
2024-10-19 19:55:26 +0200

    casa problema5UT2

--- 01_Programacion/UT2/ProgramasClaseUT2/Puebas/src/Puebas.java
+++ 01_Programacion/UT2/ProgramasClaseUT2/Puebas/src/Puebas.java
@@ -7,21 +7,83 @@ public class Puebas {
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner(System.in);
-        double valorNota = 0, sumaNota = 0, sumAprobado = 0, sumSuspenso = 0;
+        
+        double valorNota = 0, sumaNota = 0, sumAprobado=0, sumSuspenso=0, notaMayor=0, notaMenor=11, menorNotAprobado =11, mayorNotSuspenso =0;
         int contAlumnos = 0, contAprobado = 0, contSuspenso = 0;
         
+        System.out.println("----------------------------");
+        System.out.println("--  ESTADÍSTICA DE NOTAS  --");
+        System.out.println("----------------------------\n");
+        
         while(valorNota >= 0){
-           System.out.print("Introduce un valor: ");
-           while(!teclado.hasNextDouble() || (valorNota = teclado.nextDouble()) >=0 && valorNota <=10){       
-           teclado.nextLine();
-           if(valorNota >=0 && valorNota <=10){
-            System.out.println("nota" + valorNota);
-            System.out.print("Introduce un valor1: ");
-           }else{
-            System.out.print("Introduce un valor2: ");
+                                
+            System.out.print("Introduce una nota (valor negativo para salir): ");
+            while(teclado.hasNextDouble() && (valorNota = teclado.nextDouble()) >=0 && valorNota <=10){
+                teclado.nextLine();
+                contAlumnos ++;
+                sumaNota +=valorNota;
+                if(valorNota >= 5){
+                    contAprobado ++;
+                    sumAprobado += valorNota;
+                    if(notaMayor <= valorNota){
+                        notaMayor = valorNota;
+                    }                    
+                    if(menorNotAprobado > valorNota){
+                        menorNotAprobado = valorNota;
+                    }
+                }else{
+                    contSuspenso ++;
+                    sumSuspenso += valorNota;                    
+                    if(notaMenor >= valorNota){
+                        notaMenor = valorNota;
+                    }
+                    if(mayorNotSuspenso < valorNota){
+                        mayorNotSuspenso = valorNota;
+                    }
+                }                
+                System.out.print("Introduce una nota (valor negativo para salir): ");
             }
-        }
-        System.out.println("\nSiguiente parte.");
+            if(valorNota < 0){
+                teclado.nextLine();
+            }else{
+            System.out.println("ERROR!!, dato de nota no válido");
+            teclado.nextLine();
+            }
+        }       
+        if(contAlumnos == 0){
+        System.out.println("\nNo se han introducido notas.");
+        }else{
+            System.out.println("\n- Se ha evaluado un total de " + contAlumnos + " alumnos.");
+            double mediaNota = sumaNota / contAlumnos;
+            System.out.printf("- La nota media del curso ha sido un %.2f \n", mediaNota);            
+            double porceNotaApro = (100 * contAprobado)/contAlumnos;
+            System.out.print("- Han aprobado " + contAprobado);
+            System.out.printf(" alumnos, lo que supone el %.2f  del total \n", porceNotaApro);
+            double mediAprobados = sumAprobado / contAprobado;
+            if (contAprobado == 0){
+                System.out.println("- La nota media de los alumnos aprobados no existe, por que no hay ningún aprobado.");
+            }else{
+                System.out.printf("- La nota media de los alumnos aprobados es de %.2f\n", mediAprobados);
+            }            
+            double porceNotaSuspe = (100 * contSuspenso)/contAlumnos;
+            System.out.print("- Han suspendido " + contSuspenso);
+            System.out.printf(" alumnos, lo que supone el %.2f del total \n", porceNotaSuspe);
+            double mediSuspensos = sumSuspenso / contSuspenso;
+            if (contSuspenso == 0){
+                System.out.println("- La nota media de los alumnos suspensos no existe, por que no hay ningún suspendido.");
+            }else{
+                System.out.printf("- La nota media de los alumnos suspensos es de %.2f\n", mediSuspensos);
+            }
+            if(notaMayor != 0){
+                System.out.printf("- La nota más alta ha sido %.2f \n", notaMayor);
+            }else{
+                System.out.printf("- La nota más alta ha sido %.2f \n", mayorNotSuspenso);
+            }            
+           if(notaMenor != 11){
+                System.out.printf("- La nota más baja ha sido %.2f \n", notaMenor);
+           }else{
+               System.out.printf("- La nota más baja ha sido %.2f \n", menorNotAprobado);
+           }            
+        }       
     }
-}
 }
--- 01_Programacion/UT2/ProgramasClaseUT2/UT2Problema5/src/UT2Problema5.java
+++ 01_Programacion/UT2/ProgramasClaseUT2/UT2Problema5/src/UT2Problema5.java
@@ -8,33 +8,82 @@ public class UT2Problema5 {
         
         Scanner teclado = new Scanner(System.in);
         
-        double valorNota, sumaNota=0, sumAprobado=0, sumSuspenso=0;
+        double valorNota = 0, sumaNota = 0, sumAprobado=0, sumSuspenso=0, notaMayor=0, notaMenor=11, menorNotAprobado =11, mayorNotSuspenso =0;
         int contAlumnos = 0, contAprobado = 0, contSuspenso = 0;
-        System.out.print("Introduce un valor: ");       
         
-        while(teclado.hasNextDouble() && (valorNota = teclado.nextDouble()) >=0 && valorNota <=10){       
-            teclado.nextLine();
-            contAlumnos ++;
-            sumaNota += valorNota; 
-            if(valorNota >= 5){
-                contAprobado ++;
-                sumAprobado += valorNota;
+        System.out.println("----------------------------");
+        System.out.println("--  ESTADÍSTICA DE NOTAS  --");
+        System.out.println("----------------------------\n");
+        
+        while(valorNota >= 0){
+                                
+            System.out.print("Introduce una nota (valor negativo para salir): ");
+            while(teclado.hasNextDouble() && (valorNota = teclado.nextDouble()) >=0 && valorNota <=10){
+                teclado.nextLine();
+                contAlumnos ++;
+                sumaNota +=valorNota;
+                if(valorNota >= 5){
+                    contAprobado ++;
+                    sumAprobado += valorNota;
+                    if(notaMayor <= valorNota){
+                        notaMayor = valorNota;
+                    }                    
+                    if(menorNotAprobado > valorNota){
+                        menorNotAprobado = valorNota;
+                    }
+                }else{
+                    contSuspenso ++;
+                    sumSuspenso += valorNota;                    
+                    if(notaMenor >= valorNota){
+                        notaMenor = valorNota;
+                    }
+                    if(mayorNotSuspenso < valorNota){
+                        mayorNotSuspenso = valorNota;
+                    }
+                }                
+                System.out.print("Introduce una nota (valor negativo para salir): ");
+            }
+            if(valorNota < 0){
+                teclado.nextLine();
             }else{
-                contSuspenso ++;
-                sumSuspenso += valorNota;
+            System.out.println("ERROR!!, dato de nota no válido");
+            teclado.nextLine();
+            }
+        }       
+        if(contAlumnos == 0){
+        System.out.println("\nNo se han introducido notas.");
+        }else{
+            System.out.println("\n- Se ha evaluado un total de " + contAlumnos + " alumnos.");
+            double mediaNota = sumaNota / contAlumnos;
+            System.out.printf("- La nota media del curso ha sido un %.2f \n", mediaNota);            
+            double porceNotaApro = (100 * contAprobado)/contAlumnos;
+            System.out.print("- Han aprobado " + contAprobado);
+            System.out.printf(" alumnos, lo que supone el %.2f  del total \n", porceNotaApro);
+            double mediAprobados = sumAprobado / contAprobado;
+            if (contAprobado == 0){
+                System.out.println("- La nota media de los alumnos aprobados no existe, por que no hay ningún aprobado.");
+            }else{
+                System.out.printf("- La nota media de los alumnos aprobados es de %.2f\n", mediAprobados);
             }            
-            System.out.print("Introduce un valor: ");
-           
-        }
-        System.out.println("\nSiguiente parte.");
-        System.out.println("Numero de alumnos " + contAlumnos);
-        System.out.println("Media de notas " + sumaNota / contAlumnos);
-        System.out.println("Numero de aprobados " + contAprobado);
-        System.out.println("Porcentaje aprobados: " + (100 * contAprobado)/contAlumnos);
-        System.out.println("Media de Aprobados " +  sumAprobado / contAprobado);
-        System.out.println("Numero de suspensos " + contSuspenso);
-        System.out.println("Porcentaje Suspensos: " + (100 * contSuspenso)/contAlumnos);        
-        System.out.println("Media de suspensos " +  sumSuspenso /contSuspenso);
+            double porceNotaSuspe = (100 * contSuspenso)/contAlumnos;
+            System.out.print("- Han suspendido " + contSuspenso);
+            System.out.printf(" alumnos, lo que supone el %.2f del total \n", porceNotaSuspe);
+            double mediSuspensos = sumSuspenso / contSuspenso;
+            if (contSuspenso == 0){
+                System.out.println("- La nota media de los alumnos suspensos no existe, por que no hay ningún suspendido.");
+            }else{
+                System.out.printf("- La nota media de los alumnos suspensos es de %.2f\n", mediSuspensos);
+            }
+            if(notaMayor != 0){
+                System.out.printf("- La nota más alta ha sido %.2f \n", notaMayor);
+            }else{
+                System.out.printf("- La nota más alta ha sido %.2f \n", mayorNotSuspenso);
+            }            
+           if(notaMenor != 11){
+                System.out.printf("- La nota más baja ha sido %.2f \n", notaMenor);
+           }else{
+               System.out.printf("- La nota más baja ha sido %.2f \n", menorNotAprobado);
+           }            
+        }       
     }
-    
 }
