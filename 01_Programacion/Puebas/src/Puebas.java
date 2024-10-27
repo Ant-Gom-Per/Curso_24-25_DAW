235fda9770523d812fb3013677d555fbeebdc2dc
Ant-Gom-Per <antgomper79@outlook.es>
2024-10-27 19:39:34 +0100

    casa proyecto UT2 terminado

--- 01_Programacion/Puebas/src/Puebas.java
+++ 01_Programacion/Puebas/src/Puebas.java
@@ -6,21 +6,97 @@ public class Puebas {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        int valor1=0;      
-        while(valor1 >=0){
-            System.out.print("di valor1 (negativo para salir) ");
-            while(!teclado.hasNextInt() || (valor1 = teclado.nextInt()) ==0){
-                teclado.nextLine();
-                System.out.println("Error.");
-                System.out.print("di valor2 (negativo para salir) ");                
+        String palabra, ayuda="";
+        String palabraSecreta = "murcielago";
+        boolean semaforo = true, resultado= false, victoria = false;
+        int intentos=1, numeroIntentos =0, numeroPistas=0, puntuacion, continuar=0;
+        
+       while(semaforo == true){
+            System.out.print("Introduce un numero de intentos: ");              
+            while(semaforo == true){            
+                if(teclado.hasNextInt() && (intentos = teclado.nextInt()) >0 && intentos <=6 || intentos == 0){
+                    numeroIntentos = intentos;
+                    teclado.nextLine();
+                    semaforo = false;
+                }else{           
+                    System.out.println("Error");
+                    System.out.print("Introduce un numero de intentos: ");
+                    teclado.nextLine();                
+                }
             }
-            teclado.nextLine();
-        }        
-        System.out.println("KAKAPÚ");        
+
+            while(semaforo == false){            
+                System.out.print("Adivina la palabra secreta: ");
+                palabra = teclado.nextLine();        
+                resultado = palabra.equals(palabraSecreta);
+                if(resultado == false){
+                    System.out.println("Error, esa no es la palabra secreta.\n");
+                }           
+                if(resultado == true){
+                    System.out.println("Acertaste!!");
+                    semaforo = true;
+                    victoria = true;
+                }else{
+                    if(numeroIntentos -1 == numeroPistas){
+                        System.out.println("Lo siento no hay más intentos.");
+                        semaforo = true;
+                    }else{
+                    numeroPistas++;
+                    switch(numeroPistas){
+                        case 1:
+                            ayuda = "Es un animal.";
+                            break;
+                        case 2:
+                            ayuda = "Su nombre tiene 5 letras.";
+                            break;
+                        case 3:
+                            ayuda = "Es mamífero.";
+                            break;
+                        case 4:
+                            ayuda = "Las letras de su nombre son diferentes.";
+                            break;
+                        default:
+                            ayuda = "Puede volar.";                    
+                    }                
+                    System.out.println("Pista numero " + numeroPistas + ": " + ayuda);
+                    }                
+                }          
+            }
+
+            if(victoria == true){                 
+                puntuacion = (12 - (intentos+numeroPistas))*100;        
+                if(puntuacion > 1000){
+                    System.out.println("\n******************");
+                    System.out.println("**   PERFECTO   **");
+                    System.out.println("******************");            
+                }else{        
+                    System.out.println("\n**********************");
+                    System.out.println("**  Puntuacion " + puntuacion + "  **");
+                    System.out.println("**********************");
+                }
+            }else{
+                System.out.println("\n********************");
+                System.out.println("**  Puntuacion 0  **");
+                System.out.println("********************");
+            }
+
+            System.out.print("¿Quieres volver a jugar? (0 = No, 1 = Si) :");        
+            while(!teclado.hasNextInt() || (continuar = teclado.nextInt()) < 0 || continuar > 1){
+                teclado.nextLine();
+                System.out.println("Error dato no válido.");
+                System.out.println("¿Quieres volver a jugar? (0 = No, 1 = Si :");
+            }
+            if(continuar == 0){
+                semaforo = false;
+
+            }
+            numeroPistas = 0;
+       }       
+        
+        System.out.println("\n*********************");
+        System.out.println("**  Fin del juego  **");
+        System.out.println("*********************");
+       
         teclado.close();
     }    
 }
-
-
-
-// "hola".charAt()
--- 01_Programacion/UT2/problemas_evaluables/AdivinaLaPalabraVer2/src/AdivinaLaPalabraVer2.java
+++ 01_Programacion/UT2/problemas_evaluables/AdivinaLaPalabraVer2/src/AdivinaLaPalabraVer2.java
@@ -9,42 +9,119 @@ public class AdivinaLaPalabraVer2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
+        String palabra, ayuda="";
         String palabraSecreta = "murcielago";
-        boolean semaforo = true;
-        int intentos=1, intentosElegidos =0;
+        boolean semaforo = true, resultado= false, victoria = false;
+        int intentos=1, numeroIntentos =0, numeroPistas=0, puntuacion=0, continuar=0;
         
-        System.out.println("**************************");
-        System.out.println("** ADIVINA **   /)_/)   **");
-        System.out.println("**   LA    **  (^.^ )   **");
-        System.out.println("** PALABRA **  (U U )o  **");
-        System.out.println("** SECRETA **  (\")(\")   **");         
-        System.out.println("**************************");
-        System.out.println("**     COMO JUGAR       **");
-        System.out.println("***************************************************************************************");
-        System.out.println("** 1.- Para empezar a jugar has de decidir el número de intentos que vas a tener.    **");
-        System.out.println("** 2.- Una vez gastados los intentos pierdes si no has adivinado la PALABRA SECRETA. **");
-        System.out.println("** 3.- Contra menos intentos elijas, mayor será tu puntuación.                       **");
-        System.out.println("** 4.- Para salir del juego pulsa 0 en el número de intentos.                        **");
-        System.out.println("***************************************************************************************");
+         while(semaforo == true){
+            System.out.println("\n**************************");
+            System.out.println("** ADIVINA **   /)_/)   **");
+            System.out.println("**   LA    **  (^.^ )   **");
+            System.out.println("** PALABRA **  (U U )o  **");
+            System.out.println("** SECRETA **  (\")(\")   **");         
+            System.out.println("**************************");
+            System.out.println("**     COMO JUGAR       **");
+            System.out.println("****************************************************************************************");
+            System.out.println("** 1.- Para empezar a jugar debes decidir el número de intentos. (máximo 6)           **");
+            System.out.println("** 2.- Una vez gastados los intentos pierdes si no has adivinado la PALABRA SECRETA.  **");
+            System.out.println("** 3.- Contra menos intentos elijas, mayor será tu puntuación.                        **");
+            System.out.println("** 4.- Para salir del juego pulsa 0 en el número de intentos.                         **");
+            System.out.println("****************************************************************************************");
         
-        System.out.print("Introduce un numero de intentos: ");              
-        while(semaforo == true){          
-            
-            if(teclado.hasNextInt() && (intentos = teclado.nextInt()) >0 && intentos <=10 || intentos == 0){
-                intentosElegidos = intentos;
-                teclado.nextLine();
-                semaforo = false;
-            }else{           
-                System.out.println("Error");
-                System.out.print("Introduce un numero de intentos2: ");
-                teclado.nextLine();                
+       
+            System.out.print("\nIntroduce un numero de intentos: ");              
+            while(semaforo == true){               
+                if(teclado.hasNextInt() && (intentos = teclado.nextInt()) >=0 && intentos <=6){
+                    teclado.nextLine();
+                    numeroIntentos = intentos;                    
+                    semaforo = false;
+                }else{
+                    teclado.nextLine();
+                    System.out.println("Error, dato no válido.");
+                    System.out.print("\nIntroduce un numero de intentos: ");                                    
+                }
             }
-        }            
-        System.out.println("BIEN!!, Continua el programa");
-        System.out.println("Numero intentos: " + intentosElegidos);
+            
+            if(numeroIntentos == 0){
+                semaforo = true;
+            }
+
+            while(semaforo == false){            
+                System.out.print("Adivina la palabra secreta: ");
+                palabra = teclado.nextLine();        
+                resultado = palabra.equals(palabraSecreta);
+                if(resultado == false){
+                    System.out.println("Error, esa no es la palabra secreta.\n");
+                }           
+                if(resultado == true){
+                    System.out.println("Acertaste!!");
+                    semaforo = true;
+                    victoria = true;
+                }else{
+                    if(numeroIntentos -1 == numeroPistas){
+                        System.out.println("Lo siento no hay más intentos.");
+                        semaforo = true;
+                    }else{
+                    numeroPistas++;
+                    switch(numeroPistas){
+                        case 1:
+                            ayuda = "Es un animal.";
+                            break;
+                        case 2:
+                            ayuda = "Las letras de su nombre son todas diferentes.";
+                            break;
+                        case 3:
+                            ayuda = "Es mamífero.";
+                            break;
+                        case 4:
+                            ayuda = "Su nombre tiene 5 letras.";
+                            break;
+                        default:
+                            ayuda = "Puede volar.";                    
+                    }                
+                    System.out.println("Pista numero " + numeroPistas + ": " + ayuda);
+                    }                
+                }          
+            }
+            
+            if(victoria == true){                 
+                puntuacion = (12 - (intentos+numeroPistas))*100;        
+                if(puntuacion > 1000){
+                    System.out.println("\n******************");
+                    System.out.println("**   PERFECTO   **");
+                    System.out.println("******************");            
+                }else{        
+                    System.out.println("\n**********************");
+                    System.out.println("**  Puntuacion " + puntuacion + "  **");
+                    System.out.println("**********************");
+                }
+            }else{
+                System.out.println("\n********************");
+                System.out.println("**  Puntuacion 0  **");
+                System.out.println("********************");
+            }
+
+            System.out.print("¿Quieres volver a jugar? (0 = No, 1 = Si) :");        
+            while(!teclado.hasNextInt() || (continuar = teclado.nextInt()) < 0 || continuar > 1){
+                teclado.nextLine();
+                System.out.println("Error dato no válido.");
+                System.out.println("¿Quieres volver a jugar? (No = 0, Si = 1 :");
+            }
+            if(continuar == 0){
+                semaforo = false;
+                
+
+            }
+            numeroPistas = 0;
+            victoria = false;
+            
+       }       
+        
+        System.out.println("\n*********************");
+        System.out.println("**  Fin del juego  **");
+        System.out.println("*********************");
+       
+        teclado.close();
     }    
 }
-
-
-
-// "hola".charAt()
