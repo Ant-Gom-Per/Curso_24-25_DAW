5af346c68063e11fa014dc5b12f371226341d1bd
Ant-Gom-Per <ciclost@localhost>
2024-10-31 16:55:17 +0100

    clse cotinuo progUT3

--- 01_Programacion/PuebaSuper/src/PuebaSuper.java
+++ 01_Programacion/PuebaSuper/src/PuebaSuper.java
@@ -6,35 +6,67 @@ public class PuebaSuper {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        double [] arrayDatosTension = new double[20];
-        double datoTension=0;
-        int i=0, opcion;
-        boolean semaforo = true;
+        double [] arraySistolica = new double[10];
+        double [] arrayDiastolica = new double[10];
+        double datoTension=0, auxSistolica=0,auxDiastolica=0;
+        int i=0,contMedidas=0, opcion;
+        boolean semaforo = true, error = false;
+        
         
         System.out.println("TENSIÓN ARTERIAL");
-        System.out.println("----------------");
-
+        System.out.println("----------------\n");
         
-        System.out.print("Introduce los datos obtenidos: ");
-            while((datoTension=teclado.nextDouble()) !=0){
-                while(i < arrayDatosTension.length-1){
-                    if(!teclado.hasNextDouble()){
-                        teclado.next();
-                    }else if(datoTension >=3 && datoTension <=22){
-                        arrayDatosTension [i] += datoTension;
-                        i++;
-                    }
-                }
-                teclado.nextLine();
+        System.out.print("Introduce las medidas de tension: ");
+        while(contMedidas <= 10){
+            if(!teclado.hasNextDouble()){
+                error = true;
+                teclado.next();                
             }
-            teclado.nextLine();
+            if((datoTension = teclado.nextDouble()) == 0 && contMedidas >= 2){
+                semaforo = false;                
+            }
+            if(datoTension <3 || datoTension >22){
+                error = true;
+            }
+            auxSistolica = datoTension;
+            System.out.println("**********auxSIS " + auxSistolica);
+            
+            if(!teclado.hasNextDouble()){
+                teclado.next();
+            }
+            if((datoTension = teclado.nextDouble()) == 0 && contMedidas >= 2){
+                semaforo = false;
+            }
+            if(datoTension <3 || datoTension >22 || auxSistolica < datoTension){
+                error = true;
+            }
+            auxDiastolica = datoTension;
+            System.out.println("*********auxDIA " + auxDiastolica);
+            
+            if(!error == true){
+            arraySistolica[contMedidas] = auxSistolica;
+            arrayDiastolica[contMedidas] = auxDiastolica;
+            contMedidas++;
+            System.out.println("*********contador" + contMedidas);
+            }
             
             
-             System.out.print("VAlores: ");
-                for(int j=0 ; j < arrayDatosTension.length ; j++){
-                    System.out.print(arrayDatosTension[j] + " ");        
+               System.out.print("VAloresSIS: ");
+                for(int j=0 ; j <  contMedidas; j++){
+                    System.out.print( arraySistolica[j] + " ");        
                 }
                 System.out.println("");
+                
+                System.out.print("VAloresDIA: ");
+                for(int j=0 ; j <  contMedidas; j++){
+                    System.out.print( arrayDiastolica[j] + " ");        
+                }
+                System.out.println("");
+            
+        }
+        ;
+        
+        
             
             
             
@@ -46,6 +78,7 @@ public class PuebaSuper {
             System.out.println("4.- Tensión Media");
             System.out.println("5.- Salir");
             
+            semaforo = true;
             while(semaforo == true){
                 System.out.print("Introduce la opcion deseada: ");
                 while(!teclado.hasNextInt() || (opcion = teclado.nextInt()) <1 || opcion > 5){
