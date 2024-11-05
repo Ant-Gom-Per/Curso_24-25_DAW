bb538c0295fef63b6d5e1a5c365834f9df3d38e7
Ant-Gom-Per <antgomper79@outlook.es>
2024-11-05 13:07:41 +0100

    casa UT3Problema1 continuo

--- 01_Programacion/UT3/problemasEvaluables/UT3Problema1/src/UT3Problema1.java
+++ 01_Programacion/UT3/problemasEvaluables/UT3Problema1/src/UT3Problema1.java
@@ -10,22 +10,23 @@ public class UT3Problema1 {
         
         double [] arraySist = new double[10];
         double [] arrayDiast = new double[10];
-        double datoSist=0,datoDiast=0, sisMaxima=0, sisMinima = 23;
-        int contMedidas=0, opcion;
+        double datoSist=0,datoDiast=0, sisMaxima=0, diasMinima = 23, auxCompens=23, totSist=0, totDiast=0, promedioSist=0, promedioDiast=0;
+        int contMed=0, opcion=0, indArray=0;
         boolean error = false;
+        String claseTension = null;
         
         
         System.out.println("TENSIÓN ARTERIAL");
         System.out.println("----------------\n");
         
         System.out.print("Introduce las medidas de tension: ");
-        while(contMedidas < 10){
+        while(contMed < 10){
             if(!teclado.hasNextDouble()){
                 error = true;
                 teclado.next();                
             }           
             if((datoSist = teclado.nextDouble()) <3 || datoSist >22){
-                if(datoSist == 0 && contMedidas >2){
+                if(datoSist == 0 && contMed >=2){
                     break;
                 }
                 error = true;
@@ -36,51 +37,28 @@ public class UT3Problema1 {
             }
             // Hasta aqui la criba de datos de la Sistolica
             
-            if((datoDiast = teclado.nextDouble()) == 0 && contMedidas >= 1){
-                semaforo = false;
+            if(!teclado.hasNextDouble()){
                 error = true;
+                teclado.next();                
             }
-            if(datoTension <3 || datoTension >22 || datoSist < datoDiast){
+            if((datoDiast = teclado.nextDouble()) <3 || datoDiast >22){
+                  if(datoSist == 0 && contMed >=2){
+                      break;
+                  }
+                  error = true;
+              }
+            if(datoSist < datoDiast){
                 error = true;
-            }
-         
-            System.out.println("*********auxSIS " + datoSist);
-            System.out.println("*********auxDIA " + datoDiast);
-            
-            if(error == true){
-                
-                arraySist[contMedidas] = datoSist;
-                arrayDiast[contMedidas] = datoDiast;
-           
-                System.out.println("*********contador" + contMedidas);
-            
-            
-            
-               System.out.print("VAloresSIS: ");
-                for(int j=0 ; j <=  contMedidas; j++){
-                    System.out.print( arraySist[j] + " ");        
-                }
-                System.out.println("");
-                
-                System.out.print("VAloresDIA: ");
-                for(int j=0 ; j <=  contMedidas; j++){
-                    System.out.print( arrayDiast[j] + " ");        
-                }
-                System.out.println("");
-            }
-            contMedidas++;
-            if(contMedidas == 2){
-                semaforo = false;
-            }            
-        }
-        
-        
-        
-        
-            
-            
-            
-            //HASTA AQUÍ LA TOMA DE DATOS   
+            }          
+              // Hasta aquí la criba de datos Diastolica
+
+            if(error == false){                
+                arraySist[contMed] = datoSist;
+                arrayDiast[contMed] = datoDiast;
+                contMed++;
+            }         
+        }            
+        //HASTA AQUÍ LA TOMA DE DATOS   
             
         System.out.println("\n1.- Sistólica máxima");
         System.out.println("2.- Diastólica mínima");
@@ -88,69 +66,86 @@ public class UT3Problema1 {
         System.out.println("4.- Tensión Media");
         System.out.println("5.- Salir");
             
-        semaforo = true;
-        while(semaforo == true){
+     
+        while(opcion != 5){
             System.out.print("\nIntroduce la opcion deseada: ");
             while(!teclado.hasNextInt() || (opcion = teclado.nextInt()) <1 || opcion > 5){
                 teclado.nextLine();
                 System.out.println("Error, dato no válido.");
                 System.out.print("Introduce la opcion deseada: ");
             }
-            teclado.nextLine();            
+            teclado.nextLine();
+            
+            
+           
             
             switch(opcion){
                 case 1:
-                    for(int i=0 ; i < contMedidas ; i++){
-                        if(sisMaxima < arraySist[i]){ 
-                        sisMaxima = arraySist[i];            
+                    for(int i=0 ; i < contMed ; i++){
+                        if(arraySist[i] > sisMaxima){ 
+                            sisMaxima = arraySist[i];
+                            indArray = i;
                         }
                     }
-                    if(sisMaxima < 12){
-                        System.out.printf("%.1f Óptima\n", sisMaxima);
-                    }else if(sisMaxima >= 12 && sisMaxima <12.9){
-                        System.out.printf("%.1f Normal\n", sisMaxima);
-                    }else if(sisMaxima >= 13 && sisMaxima <13.9){
-                        System.out.printf("%.1f Normal-Alta\n", sisMaxima);
-                    }else if(sisMaxima >= 14 && sisMaxima <15.9){
-                        System.out.printf("%.1f Grado 1\n", sisMaxima);
-                    }else if(sisMaxima >= 16 && sisMaxima <17.9){
-                        System.out.printf("%.1f Grado 2\n", sisMaxima);
-                    }else{
-                        System.out.printf("%.1f Grado 3\n", sisMaxima);
-                    }                        
+                    System.out.print(sisMaxima + " " + arrayDiast[indArray]);   
                     break;
+                    
                 case 2:
-                    for(int i=0 ; i < contMedidas ; i++){
-                        if(sisMinima > arrayDiast[i]){ 
-                        sisMinima = arrayDiast[i];            
+                    for(int i=0 ; i < contMed ; i++){
+                        if(arrayDiast[i] < diasMinima){ 
+                            diasMinima = arrayDiast[i];
+                            indArray = i;
                         }
                     }
-                    if(sisMinima < 8){
-                        System.out.printf("%.1f Óptima\n", sisMinima);
-                    }else if(sisMinima >= 8 && sisMinima <8.4){
-                        System.out.printf("%.1f Normal\n", sisMinima);
-                    }else if(sisMinima >= 8.5 && sisMinima <8.9){
-                        System.out.printf("%.1f Normal-Alta\n", sisMinima);
-                    }else if(sisMinima >= 9 && sisMinima <9.9){
-                        System.out.printf("%.1f Grado 1\n", sisMinima);
-                    }else if(sisMinima >= 10 && sisMinima <10.9){
-                        System.out.printf("%.1f Grado 2\n", sisMinima);
-                    }else{
-                        System.out.printf("%.1f Grado 3\n", sisMinima);
-                    }
+                    System.out.print(arraySist[indArray] + " " + diasMinima);
                     break;
+                    
                 case 3:
-                    System.out.println("opcion3");
+                    for(int i =0; i < contMed ; i++){
+                        if(Math.abs(arraySist[i] - (arrayDiast[i] *2)) < auxCompens){
+                            auxCompens = Math.abs(arraySist[i] - (arrayDiast[i] *2));
+                            indArray = i;
+                        }
+                    }
+                    System.out.print(arraySist[indArray] + " " + arrayDiast[indArray]);
                     break;
                 case 4:
-                    System.out.println("opcion4");
+                    for(int i=0 ; i<contMed ; i++){
+                        totSist += arraySist[i];
+                    }
+                    for(int i=0 ; i<contMed ; i++){
+                        totDiast += arrayDiast[i];
+                    }
+                    promedioSist = totSist/contMed;
+                    promedioDiast = totDiast/contMed;
+                    System.out.printf("Dato de tension promedio: %.1f %.1f\n",promedioSist, promedioDiast);
+                    
                     break;
                 default:
-                    System.out.println("salir");
-                    semaforo = false;
+                    System.out.println("Saliendo del programa...");
+                   
                     break;
             }
-
+            
+            if(opcion ==4 || opcion ==5)
+                if (arraySist[indArray] < 12 && arrayDiast[indArray] < 8) {
+                        claseTension = " Óptima";
+                    } else if (arraySist[indArray] >= 12 && arraySist[indArray] < 13 && arrayDiast[indArray] >= 8 && arrayDiast[indArray] < 8.5) {
+                        claseTension = " Normal";
+                    } else if (arraySist[indArray] >= 13 && arraySist[indArray] < 14 && arrayDiast[indArray] >= 8.5 && arrayDiast[indArray] < 9) {
+                        claseTension = " Normal - Alta";
+                    } else if (arraySist[indArray] >= 14 && arraySist[indArray] < 16 && arrayDiast[indArray] >= 9 && arrayDiast[indArray] < 10) {
+                        claseTension = " Grado 1";
+                    } else if (arraySist[indArray] >= 16 && arraySist[indArray] < 18 && arrayDiast[indArray] >= 10 && arrayDiast[indArray] < 11) {
+                        claseTension = " Grado 2";
+                    } else if (arraySist[indArray] >= 18 && arrayDiast[indArray] >= 11) {
+                        claseTension = " Grado 3";
+                    } else if (arraySist[indArray] >= 14 && arrayDiast[indArray] < 9) {
+                        claseTension = " Sistolica Aislada";
+                    } else {
+                        claseTension = " No contemplada";
+                    }
+            System.out.println(" " + claseTension);
                 //  HASTA AQUÍ LAS POSIBLES OPCIONES              
         }        
         System.out.println("Fin del programa");
