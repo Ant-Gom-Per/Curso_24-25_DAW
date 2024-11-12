85e8bc0862dec3aa14746e01f27abc006396efb8
Ant-Gom-Per <ciclost@localhost>
2024-11-12 16:51:32 +0100

    clase repaso prog

--- 01_Programacion/BancoDePruebas/src/BancoDePruebas.java
+++ 01_Programacion/BancoDePruebas/src/BancoDePruebas.java
@@ -7,22 +7,43 @@ public class BancoDePruebas {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int[][] cantDivis = new int [0][4];
-        int[][] aux;
-        int dividendo=0;
         
-        System.out.print("Introduce las divisiones (-1 para terminar): ");
-        while(dividendo != -1){
-           aux = cantDivis;
-           dividendo = teclado.nextInt();
-           
-                       
+        
+        int valor1 = 0, valor2 =0;
+        valor1 =(int)( Math.random() * 1 +3);
+        int[][] matrizReales = new int[valor1][valor1];
+        int[][] matrizReales2 = new int[valor1][valor1];
+        int[][] matrizReales3 = new int[valor1][valor1];
+        
+        
+        for (int i = 0; i < matrizReales.length; i++) {
+            for (int j = 0; j < matrizReales[i].length; j++) {                
+                matrizReales[i][j] += valor2 =(int)( Math.random() * 10);
+                System.out.print(matrizReales[i][j] + "\t"); 
+            }
+            System.out.println(" ");
         }
-       
         
+        System.out.println("\n");
+        for (int i = 0; i < matrizReales2.length; i++) {
+            for (int j = 0; j < matrizReales2[i].length; j++) {                
+                matrizReales2[i][j] += valor2 =(int)( Math.random() * 10);
+                System.out.print(matrizReales2[i][j] + "\t"); 
+            }
+            System.out.println(" ");
+        }
+        
+        System.out.println("\n");
+        for (int i = 0; i < matrizReales3.length; i++) {
+            for (int j = 0; j < matrizReales3[i].length; j++) {            
+                matrizReales3[i][j] += (matrizReales[i][j] + matrizReales2[i][j]);
+                System.out.print(matrizReales3[i][j] + "\t"); 
+            }
+            System.out.println(" ");
+        }
         
         System.out.println("--------------------------------------------------");
-        System.out.println("dividendo: " + dividendo);
+        System.out.println("valor1:" + valor1);
         teclado.close();
     }
     
