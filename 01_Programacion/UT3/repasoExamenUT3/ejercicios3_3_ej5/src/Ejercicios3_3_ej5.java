b199bbf224f0752b6a4dd5ebccc8fe090866c7f7
Ant-Gom-Per <antgomper79@outlook.es>
2024-11-14 12:56:21 +0100

    casa repaso examenProg

---
+++ 01_Programacion/UT3/repasoExamenUT3/ejercicios3_3_ej5/src/Ejercicios3_3_ej5.java
@@ -0,0 +1,37 @@
+/*
+Ejercicio3_3_ej5
+ */
+import java.util.Scanner;
+public class Ejercicios3_3_ej5 {
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int[][] divisiones = new int[0][4];
+        int[][] aux;
+        int dividendo;
+        
+        System.out.println("Introduce las divisiones: ");
+        do {
+            aux = divisiones;          
+            dividendo = teclado.nextInt();
+            if(dividendo != -1){
+                divisiones =new int[aux.length +1][4];
+                for (int i = 0; i < aux.length; i++) {
+                    for (int j = 0; j < aux[i].length; j++) {
+                        divisiones[i] = aux[i];
+                        
+                        
+                    }
+                    
+                }
+            }
+         
+            
+        } while (true);
+        
+        
+        
+        teclado.close();
+    }
+    
+}
---
+++ 01_Programacion/ejercicios3_3_4/src/Ejercicios3_3_4.java
@@ -0,0 +1,44 @@
+/*
+Ejercicios3_3 ejercicio nº 4
+ */
+import java.util.Scanner;
+public class Ejercicios3_3_4 {
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        boolean simetrica = true;
+        int longitud;
+        System.out.print("Introduce la longitud de la matriz: ");
+        while(!teclado.hasNextInt() || (longitud = teclado.nextInt()) < 1){
+            teclado.nextLine();
+            System.out.println("Error, dato no válido: ");
+            System.out.print("Introduce los valores de la matríz: ");
+        }
+        teclado.nextLine();
+        
+        System.out.println("Introduce los valores de la matriz: ");
+        int[][] matriz = new int [longitud][longitud];
+        for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    matriz[i][j] = teclado.nextInt();                
+            }            
+        }
+        for (int i = 0; simetrica &&  i < matriz.length; i++) {
+            for (int j = 0; simetrica && j < i; j++) {
+                if (matriz[i][j] != matriz[j][i]) {
+                    simetrica = false;
+                }                
+            }            
+        }
+        System.out.println("simetrica: " + simetrica);
+        
+        
+        System.out.println("---------------------------------------------");
+        System.out.println("longitud: " + longitud);
+        System.out.println("tamaño matriz: " + matriz.length);
+        System.out.println("datos a introducir: " + (longitud * longitud));
+        
+        teclado.close();
+    }
+    
+}
