542718e190fde410e79aa930435fd4c50395c5f4
Ant-Gom-Per <antgomper79@outlook.es>
2024-11-18 16:11:51 +0100

    casa continuo adivinapalabra_Ver3

--- 01_Programacion/BancoDePruebas/src/BancoDePruebas.java
+++ 01_Programacion/BancoDePruebas/src/BancoDePruebas.java
@@ -1,5 +1,8 @@
 /*
 Banco de pruebas para desarrollar programas
+secreta[posPalabra].charAt(posLetra -1)
+palabra.equals(palabraSecreta)
+
  */
 
 import java.util.Scanner;
@@ -8,42 +11,81 @@ public class BancoDePruebas {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
+        boolean resultado = false;
+        //int puntos=1;
+        //int[] arrayPuntos = new int[puntos];
+        int numAleat = (int)(Math.random()*10);                         // creo numero aleatorio  
+         
+        String palaIntrod = " ";        
+        String[] palabrSecr = {"perro", "gaviota", "rana", "abeja", "serpiente", "atun", "cangrejo", "escolopendra", "almeja", "tarantula"};
+        String[] resp1 = {"mamífero", "ave", "anfibio", "insecto", "reptil", "pez", "crustaceo", "miriápodo", "molusco", "aracnido"};
         
-        int valor1 = 0, valor2 =0;
-        valor1 =(int)( Math.random() * 1 +3);
-        int[][] matrizReales = new int[valor1][valor1];
-        int[][] matrizReales2 = new int[valor1][valor1];
-        int[][] matrizReales3 = new int[valor1][valor1];
         
         
-        for (int i = 0; i < matrizReales.length; i++) {
-            for (int j = 0; j < matrizReales[i].length; j++) {                
-                matrizReales[i][j] += valor2 =(int)( Math.random() * 10);
-                System.out.print(matrizReales[i][j] + "\t"); 
+       
+        
+        
+        int cantLetras = palabrSecr[numAleat].length();                 // cuento numero de letras d la palabra elegida
+        char[] letrasPalSecr = new char[cantLetras];                    // array para almacenar letras del tamano d la palabra eleguida
+        for (int i = 0; i < palabrSecr[numAleat].length(); i++) {       //
+             letrasPalSecr[i] += palabrSecr[numAleat].charAt(i);   //Cuento el numero de letras de la palabra secreta y las guardo 1 a 1 en una array                  
+        }                                                               //
+        System.out.println(" ");                                      //
+        
+        
+        System.out.println("animal: " + palabrSecr[numAleat]);          // comprobacion
+        
+        System.out.print("El animal secreto es: ");                 //inicio
+        for (int i = 0; i < cantLetras; i++) {
+            System.out.print("_ ");
+        }
+        System.out.print("\nIntroduce palabra: ");                      //
+        palaIntrod = teclado.next();                                    //
+        int cantLetras2 = palaIntrod.length();                          // cuento numero de lteras d la palabra introducida
+        char[] letrasPalIntrod = new char[cantLetras2];                 //        
+        for (int i = 0; i < palaIntrod.length(); i++) {                 //
+             letrasPalIntrod[i] += palaIntrod.charAt(i);           //Cuento el numero de letras de la palabra introducida y las guardo 1 a 1 en una array                  
+        }                                                               //
+        System.out.println(" ");                                      //
+        
+        
+        if(!palabrSecr[numAleat].equals(palaIntrod)){
+            if(cantLetras == cantLetras2){                                                 //// if palabra secreat != palabra elegida
+                for (int i = 0; i < palabrSecr[numAleat].length(); i++) {                  //
+                    if(letrasPalSecr[i] == (letrasPalIntrod[i])){                          // Comparo letras e impromo la letra si coincide y un guion si no
+                        System.out.print(palabrSecr[numAleat].charAt(i) + " ");       //          
+                    }else{                                                                 //
+                        System.out.print("_ ");                                          //
+                    }                                                                      //
+                }
+            }else{
+                System.out.println("Error, no tiene la misma cantidad letras");
             }
-            System.out.println(" ");
+        }else{            
+            resultado = true;
         }
         
-        System.out.println("\n");
-        for (int i = 0; i < matrizReales2.length; i++) {
-            for (int j = 0; j < matrizReales2[i].length; j++) {                
-                matrizReales2[i][j] += valor2 =(int)( Math.random() * 10);
-                System.out.print(matrizReales2[i][j] + "\t"); 
-            }
-            System.out.println(" ");
-        }
+        //resultado = palabrSecr[numAleat].equals(palaIntrod);// // if palabra secreat == palabra elegida resultado=true
         
-        System.out.println("\n");
-        for (int i = 0; i < matrizReales3.length; i++) {
-            for (int j = 0; j < matrizReales3[i].length; j++) {            
-                matrizReales3[i][j] += (matrizReales[i][j] + matrizReales2[i][j]);
-                System.out.print(matrizReales3[i][j] + "\t"); 
-            }
-            System.out.println(" ");
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("aleatorio: " + numAleat);
+        System.out.println("animal: " + palabrSecr[numAleat]);
+        System.out.println("Tamaño palabra: "  + cantLetras);
+        System.out.println("tipo: " + resp1[numAleat]);
+        System.out.print("palabra: ");
+        for (int i = 0; i < palabrSecr[numAleat].length(); i++) {
+            System.out.print(letrasPalSecr[i] + " ");                       
         }
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("palaIntrod: " + palaIntrod);
+        System.out.println("cantLetras2: " + cantLetras2);
+        System.out.print("palabra2: ");
+        for (int i = 0; i < palaIntrod.length(); i++) {
+            System.out.print(letrasPalIntrod[i] + " ");                       
+        }
+        System.out.println("\nresultado: " + resultado);
+       
         
-        System.out.println("--------------------------------------------------");
-        System.out.println("valor1:" + valor1);
         teclado.close();
     }
     
---
+++ 01_Programacion/UT3/problemasEvaluables/AdivinaLaPalabraVer3/src/AdivinaLaPalabraVer3.java
@@ -0,0 +1,172 @@
+/** *
+ * @author Antonio Gómez Pérez
+ * Este programa te pide que adivines una palabra.
+ */
+import java.util.Scanner;
+
+public class AdivinaLaPalabraVer3 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        String palaIntrod, ayuda="";
+        String[] palabrSecr = {"perro", "gaviota", "rana", "abeja", "serpiente", "atun", "cangrejo", "escolopendra", "almeja", "tarantula"};
+        
+        boolean semaforo = true, resultado= false, victoria = false;
+        int intentos=1, numeroIntentos =0, numeroPistas=0, puntuacion=0, continuar=0;
+        int numAleat = (int)(Math.random()*10);
+        
+         while(semaforo == true){
+            System.out.println("\n**************************");
+            System.out.println("** ADIVINA **   /)_/)   **");
+            System.out.println("**   EL    **  (^.^ )   **");
+            System.out.println("** ANIMAL  **  (U U )o  **");
+            System.out.println("** SECRETO **  (\")(\")   **");         
+            System.out.println("**************************");
+            System.out.println("**     COMO JUGAR       **");
+            System.out.println("****************************************************************************************");
+            System.out.println("** 1.- Para empezar a jugar debes decidir el número de intentos. (máximo 6)           **");
+            System.out.println("** 2.- Una vez gastados los intentos pierdes si no has adivinado el ANIMAL SECRETO.   **");
+            System.out.println("** 3.- Contra menos intentos elijas, mayor será tu puntuación.                        **");
+            System.out.println("** 4.- Para salir del juego pulsa 0 en el número de intentos.                         **");
+            System.out.println("****************************************************************************************");
+        
+       
+            System.out.print("\nIntroduce un numero de intentos: ");              
+            while(semaforo == true){               
+                if(teclado.hasNextInt() && (intentos = teclado.nextInt()) >=0 && intentos <=6){
+                    teclado.nextLine();
+                    numeroIntentos = intentos;                    
+                    semaforo = false;
+                }else{
+                    teclado.nextLine();
+                    System.out.println("Error, dato no válido.");
+                    System.out.print("\nIntroduce un numero de intentos: ");                                    
+                }
+            }
+            
+            if(numeroIntentos == 0){
+                semaforo = true;
+            }
+            // Generar numero aleatorio
+            while(semaforo == false){            
+                
+                int cantLetras = palabrSecr[numAleat].length();                 // cuento numero de letras d la palabra elegida
+                char[] letrasPalSecr = new char[cantLetras];                    // array para almacenar letras del tamano d la palabra eleguida
+                for (int i = 0; i < palabrSecr[numAleat].length(); i++) {       //
+                     letrasPalSecr[i] += palabrSecr[numAleat].charAt(i);   //Cuento el numero de letras de la palabra secreta y las guardo 1 a 1 en una array                  
+                }                                                               //
+                System.out.println(" ");                                      //
+
+
+                System.out.println("animal: " + palabrSecr[numAleat]);          // comprobacion
+                
+
+                System.out.print("El animal secreto es: ");                 //inicio
+                for (int i = 0; i < cantLetras; i++) {
+                    System.out.print("_ ");
+                }
+                
+                System.out.print("\nIntroduce palabra: ");                      //
+                palaIntrod = teclado.next();                                    //
+                int cantLetras2 = palaIntrod.length();                          // cuento numero de lteras d la palabra introducida
+                char[] letrasPalIntrod = new char[cantLetras2];                 //        
+                for (int i = 0; i < palaIntrod.length(); i++) {                 //
+                     letrasPalIntrod[i] += palaIntrod.charAt(i);           //Cuento el numero de letras de la palabra introducida y las guardo 1 a 1 en una array                  
+                }                                                               //
+                System.out.println("");                                      //
+
+
+                if(!palabrSecr[numAleat].equals(palaIntrod)){
+                    if(cantLetras == cantLetras2){                                                 //// if palabra secreat != palabra elegida
+                        System.out.print("Has acertado las letras: ");
+                        for (int i = 0; i < palabrSecr[numAleat].length(); i++) {                  //
+                            if(letrasPalSecr[i] == (letrasPalIntrod[i])){                          // Comparo letras e impromo la letra si coincide y un guion si no
+                                System.out.print(palabrSecr[numAleat].charAt(i) + " ");       //          
+                            }else{                                                                 //
+                                System.out.print("_ ");                                          //
+                            }                                                                      //
+                        }
+                    }else{
+                        System.out.println("Error, no tiene la misma cantidad letras");
+                    }
+                }else{            
+                    resultado = true;
+                }
+                
+                if(resultado == false){
+                    System.out.println("\n\nError, ese no es el animal secreto.\n");
+                }           
+                if(resultado == true){
+                    System.out.println("Acertaste!!");
+                    semaforo = true;
+                    victoria = true;
+                }else{
+                    if(numeroIntentos -1 == numeroPistas){
+                        System.out.println("Lo siento no hay más intentos.");
+                        semaforo = true;
+                    }else{
+                    numeroPistas++;
+                    switch(numeroPistas){
+                        case 1:
+                            ayuda = "Es un animal.";//  Terreste, marino, volador
+                            break;
+                        case 2:
+                            ayuda = "Las letras de su nombre son todas diferentes.";//Vertebrado o invertebrado
+                            break;
+                        case 3:
+                            ayuda = "Es mamífero.";// cambiar por print con tipo animal
+                            break;
+                        case 4:
+                            ayuda = "Su nombre tiene 5 letras.";//Tiene x patas
+                            break;
+                        default:
+                            ayuda = "Puede volar.";                 
+                    }                
+                    System.out.println("Pista numero " + numeroPistas + ": " + ayuda);
+                    }                
+                }          
+            }
+            // comparacion de palabra introducida y palabra secreta
+            
+            if(victoria == true){                 
+                puntuacion = (12 - (intentos+numeroPistas))*100;        //añadir multiplicador por palabra
+                if(puntuacion > 1000){
+                    System.out.println("\n******************");
+                    System.out.println("**   PERFECTO   **");
+                    System.out.println("******************");            
+                }else{        
+                    System.out.println("\n**********************");
+                    System.out.println("**  Puntuacion " + puntuacion + "  **");
+                    System.out.println("**********************");
+                }
+            }else{
+                System.out.println("\n********************");
+                System.out.println("**  Puntuacion 0  **");
+                System.out.println("********************");
+            }
+            //insertar puntuacion de intentos
+            
+            System.out.print("¿\nQuieres volver a jugar? (0 = No, 1 = Si) :");        
+            while(!teclado.hasNextInt() || (continuar = teclado.nextInt()) < 0 || continuar > 1){
+                teclado.nextLine();
+                System.out.println("Error dato no válido.");
+                System.out.println("¿\nQuieres volver a jugar? (No = 0, Si = 1 :");
+            }
+            if(continuar == 0){
+                semaforo = false;
+             
+
+            }
+            numeroPistas = 0;
+            victoria = false;
+            
+       }       
+        
+        System.out.println("\n*********************");
+        System.out.println("**  Fin del juego  **");
+        System.out.println("*********************");
+       
+        teclado.close();
+    }    
+}
