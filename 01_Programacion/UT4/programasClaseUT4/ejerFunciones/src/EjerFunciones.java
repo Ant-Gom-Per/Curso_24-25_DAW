f74f7686cdecf3c71bb9f407712aadf2bc303158
Ant-Gom-Per <ciclost@localhost>
2024-11-21 19:48:07 +0100

    clase Prog e ingles

--- 01_Programacion/UT4/programasClaseUT4/ejerFunciones/src/EjerFunciones.java
+++ 01_Programacion/UT4/programasClaseUT4/ejerFunciones/src/EjerFunciones.java
@@ -16,31 +16,31 @@ public class EjerFunciones {
     }
     public void inicio(){
        leerEntero();        //
-       leerEntero(0);       //En estos metodos 
+       leerEntero(0);       //En estos metodos dependiendo delos argumentos que introduces te lleva a un metodo u otro.
        leerEntero(0, 20);   //  
        
     }
     
-    public int leerEntero(){
+    public int leerEntero(){// Estre primero permite cualquier numero entero por teclado
         int entero = 0;
-        System.out.print("Introduce un entero: ");
+        System.out.print("Introduce un entero1: ");
         while(!teclado.hasNextInt()){
             teclado.nextLine();
-            System.out.println("Error, dato no válido.");
-            System.out.print("Introduce un entero: ");
+            System.out.println("Error, dato no válido1.");
+            System.out.print("Introduce un entero1: ");
         }
         entero = teclado.nextInt();
         teclado.nextLine();
         return entero;
     }
     
-    public int leerEntero(int minimo){
+    public int leerEntero(int minimo){// Estre primero permite cualquier numero entero por teclado con un parametro mínimo.
         int entero;
-        System.out.print("Introduce un entero: ");
+        System.out.print("Introduce un entero2: ");
         while(!teclado.hasNextInt() || (entero = teclado.nextInt())< minimo){
             teclado.nextLine();
-            System.out.println("Error, dato no válido.");
-            System.out.print("Introduce un entero: ");
+            System.out.println("Error, dato no válido2.");
+            System.out.print("Introduce un entero2: ");
         }        
         teclado.nextLine();
         return entero;
@@ -48,11 +48,11 @@ public class EjerFunciones {
     
     public int leerEntero(int minimo, int maximo){
         int entero;
-        System.out.print("Introduce un entero: ");
+        System.out.print("Introduce un entero3: ");
         while(!teclado.hasNextInt() || (entero = teclado.nextInt())< minimo || entero >= maximo) {
             teclado.nextLine();
-            System.out.println("Error, dato no válido.");
-            System.out.print("Introduce un entero: ");
+            System.out.println("Error, dato no válido3.");
+            System.out.print("Introduce un entero3: ");
         }        
         teclado.nextLine();
         return entero;
