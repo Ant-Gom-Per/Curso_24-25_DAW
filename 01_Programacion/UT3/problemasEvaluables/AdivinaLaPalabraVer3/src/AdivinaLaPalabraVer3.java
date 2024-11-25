acde88802e581674149b1217045ed7a75b211ac3
Ant-Gom-Per <antgomper79@outlook.es>
2024-11-25 16:06:12 +0100

    casa cosas lemaUT2

--- 01_Programacion/UT3/problemasEvaluables/AdivinaLaPalabraVer3/src/AdivinaLaPalabraVer3.java
+++ 01_Programacion/UT3/problemasEvaluables/AdivinaLaPalabraVer3/src/AdivinaLaPalabraVer3.java
@@ -28,7 +28,7 @@ public class AdivinaLaPalabraVer3 {
             System.out.println("**************************");
             System.out.println("**     COMO JUGAR       **");
             System.out.println("****************************************************************************************");
-            System.out.println("** 1.- Para empezar a jugar debes decidir el número de intentos. (máximo 6)           **");
+            System.out.println("** 1.- Para empezar a jugar debes decidir el número de intentos. (máximo 5)           **");
             System.out.println("** 2.- Una vez gastados los intentos pierdes si no has adivinado el ANIMAL SECRETO.   **");
             System.out.println("** 3.- Contra menos intentos elijas, mayor será tu puntuación.                        **");
             System.out.println("** 4.- Para salir del juego pulsa 0 en el número de intentos.                         **");
