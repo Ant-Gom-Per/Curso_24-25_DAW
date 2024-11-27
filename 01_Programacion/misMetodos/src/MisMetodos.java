2c822c6fbecb470cf59d2d5e72c60fa195518fc7
antgomper <antoniogomez.alu@iespacomolla.es>
2024-11-27 19:44:29 +0100

    lokal inicio misMetodos prog

---
+++ 01_Programacion/misMetodos/src/MisMetodos.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author antonio
+ */
+public class MisMetodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MisMetodos programa = new MisMetodos();
+        programa.inicio();
+    }
+    /**
+     * Este metodo cordina al resto
+     */
+    public void inicio(){
+        
+    }
+    
+    public int metSuma(int valor1,  int valor2){
+        
+    }
+    
+    
+}
