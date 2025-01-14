1ea466bab2649119fc55b16dce8db80b0cc8a221
Ant-Gom-Per <ciclost@localhost>
2025-01-14 20:45:25 +0100

    clase ejer tema3

--- 02_Base/UT3/ejemplo consultas/TablaConsultas.sql
+++ 02_Base/UT3/ejemplo consultas/TablaConsultas.sql
@@ -1,2 +1,2 @@
-SELECT * FROM preciosum;
-SELECT numpieza, numvend, descuento FROM preciosum ORDER BY descuento DESC;
+--13. Números de pedido y números de vendedor, para los pedidos solicitados el 15 de octubre de 1992.
+SELECT numpedido, numvend FROM pedido WHERE fecha = TO_DATE('15/10/1992', 'DD/MM/YYYY');
