330ac8f5d7571a020cfcea14dc22118ad8c5b432
Ant-Gom-Per <ciclost@localhost>
2025-03-10 20:42:41 +0100

    clase proyecto y lema

---
+++ 03_Lema/UT4/01_Ejercicios/02_ejercicio.js
@@ -0,0 +1,8 @@
+alert("Hola!");
+function mifuncion(){
+    alert("Esto es una alerta");
+}
+document.write("Hola!");
+confirm("¿Quieres continuar?");
+prompt("Introduce tu edad");
+console.log("Escrbe en la consola");//ESto aparece en la consola del navegador
