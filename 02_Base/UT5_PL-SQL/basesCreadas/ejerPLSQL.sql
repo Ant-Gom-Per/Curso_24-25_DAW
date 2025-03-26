eebcc069eba415f84d5ce1d1af992c9f2e81f580
Ant-Gom-Per <ciclost@localhost>
2025-03-26 20:45:56 +0100

    Clase prog, base y lema

--- 02_Base/UT5_PL-SQL/basesCreadas/ejerPLSQL.sql
+++ 02_Base/UT5_PL-SQL/basesCreadas/ejerPLSQL.sql
@@ -1,20 +1,26 @@
 set SERVEROUTPUT on;
 
+--Crea un programa que procesa una transacción bancaria. Antes de permitir retirar 500€ 
+--de la cuenta 3, se asegura de que la cuenta tenga fondos suficientes para cubrir la retirada. 
+--Si los fondos están disponibles, el programa debita la cuenta. De lo contrario, 
+--el programa inserta un registro en una tabla de auditoría.
+
 DECLARE
-    v_nombre VARCHAR2(50);
-    v_apellido1 VARCHAR2(50);
-    v_apellido2 VARCHAR2(50);
-    v_email VARCHAR2(100);
-    v_anyo_nac number;
-    v_telef NUMBER(9);
+    v_cuenta_1 NUMBER;
+    v_cuenta_2 NUMBER;
+    v_cuenta_3 NUMBER;       
 
 BEGIN
-    v_nombre := 'Benito';
-    v_apellido1 := 'Camelas';
-    v_apellido2 := 'Bolas';
-    v_anyo_nac := 1965;
-    dbms_output.put_line('Nombre: ' || v_nombre || ' '|| v_apellido1 || ' ' || v_apellido2 || ' ' || v_anyo_nac);   
-    dbms_output.put_line( 'Correo: ' || lower(v_apellido1) || '_' || lower(v_apellido2) || '@iespacomolla.es');   
-
+    v_cuenta_1 := 200;
+    v_cuenta_2 := 900;
+    v_cuenta_3 := 600;
     
+    IF (v_cuenta_3 - 500) < 0 THEN          
+        dbms_output.put_line('Error, no hay saldo suficiente.');
+    ELSE
+        v_cuenta_3 := v_cuenta_3 - 500;
+        dbms_output.put_line('Su saldo actual es: '|| v_cuenta_3 || ' €');
+    END IF;
+        
 END;
+
