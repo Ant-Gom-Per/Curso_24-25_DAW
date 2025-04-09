c3f7f69e63ff3f182c9e5c908f1cb1067edc3c25
Ant-Gom-Per <antgomper79@outlook.es>
2025-04-09 17:02:54 +0200

    clase OM progUT10

--- 01_Programacion/UT10/basesCreadas/JAVAUSER.sql
+++ 01_Programacion/UT10/basesCreadas/JAVAUSER.sql
@@ -1,3 +1,39 @@
-select * from prova;
-insert into prova VALUES(3,'Benito');
-insert into prova VALUES(4,'null');
+--Borrado de tablas
+DROP TABLE Empleats CASCADE CONSTRAINTS;
+DROP TABLE Oficines CASCADE CONSTRAINTS;
+
+--Creado de tablas
+CREATE TABLE Empleats(
+    numemp NUMBER(20),
+    nom VARCHAR2(100),
+    edat NUMBER,
+    oficina NUMBER(20),
+    ocupacio VARCHAR2(200),
+    contracte DATE
+);
+
+CREATE TABLE Oficines(
+    oficina NUMBER(20),
+    ciutat VARCHAR2(100),
+    superficie number(20),
+    vendes NUMBER(20)
+);
+
+--Inserción de claves primárias
+
+ALTER TABLE Empleats ADD CONSTRAINT pk_Empleats PRIMARY KEY (numemp);
+ALTER TABLE Oficines ADD CONSTRAINT pk_Oficines PRIMARY KEY (Oficina);
+
+--Inserción claves ajenas
+ALTER TABLE Empleats ADD CONSTRAINT fk_Empleats_Oficina FOREIGN KEY (Oficina) REFERENCES Oficines(oficina);
+
+
+
+
+SELECT * FROM Empleats;
+SELECT * FROM Oficines;
+
+INSERT INTO Empleats VALUES (1,'Benito Camelas Bolas', 45,23, 'Encarcago de limpieza', to_date('04/02/2015','DD/MM/YYYY'));
+INSERT INTO Empleats VALUES (2,'Elena Nito del Bosque', 33,23, 'Secretaria', to_date('04/02/2015','DD/MM/YYYY'));
+
+INSERT INTO Oficines VALUES (23,'Lepe', 500, 1000000);
